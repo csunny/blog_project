baac3aede656ff950483931202029e4e93436c38
csunny <cfqsunny@163.com>
2016-09-07 20:48:18 +0800

    update

--- magic/apps/blog/models.py
+++ magic/apps/blog/models.py
@@ -58,7 +58,7 @@ class ArticleManager(models.Manager):
         distinct_date_list = []
         date_list = self.values('date_publish')
         for date in date_list:
-            date = date['date_publish'].strftime(u'%Y/%m文章存档')
+            date = date['date_publish'].strftime('%Y/%m文章存档')
             if date not in distinct_date_list:
                 distinct_date_list.append(date)
         return distinct_date_list
