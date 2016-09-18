d76103c6f783fda093e7dea8e94efc63a2524114
csunny <cfqsunny@163.com>
2016-09-18 20:19:17 +0800

    article and user admin config

--- source/apps/blog/admin/article.py
+++ source/apps/blog/admin/article.py
@@ -6,7 +6,32 @@
 from blog.models import Article, Tag, Category, Comment
 from django.contrib import admin
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'desc', 'click_count',)
+    list_display_links = ('title', 'desc', )
+    list_editable = ('click_count',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'desc', 'content', 'user', 'category', 'tag', )
+        }),
+        ('高级设置', {
+            'classes': ('collapse',),
+            'fields': ('click_count', 'is_recommend',)
+        }),
+    )
+
+    class Media:
+        js = (
+            '/static/js/kindeditor-4.1.10/kindeditor-min.js',
+            '/static/js/kindeditor-4.1.10/lang/zh_CN.js',
+            '/static/js/kindeditor-4.1.10/config.js',
+        )
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Tag)
 admin.site.register(Comment)
 admin.site.register(Category)
--- source/apps/blog/admin/user.py
+++ source/apps/blog/admin/user.py
@@ -5,4 +5,24 @@
 """
 from django.contrib import admin
 from blog.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext, ugettext_lazy as _
 
+
+class BlogUserAdmin(UserAdmin):
+    filesets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Personal info'), {'fields': ('email', 'qq', 'phone')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': {'last_login', 'date_joined'}}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, BlogUserAdmin)
